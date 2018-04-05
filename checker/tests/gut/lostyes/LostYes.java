91e43999c11434d0f5d72ce4169b6d7d15e226a4
Mier Ta <topnessman@sina.com>
2018-04-05 10:21:27 -0400

    Revert GUT

--- checker/tests/gut/lostyes/LostYes.java
+++
@@ -1,5 +0,0 @@
-import org.checkerframework.checker.gut.qual.*;
-
-public class LostYes {
-    @Lost Object lo;
-}
--- checker/tests/gut/topol/CondExpr.java
+++
@@ -1,8 +0,0 @@
-import org.checkerframework.checker.gut.qual.*;
-
-public class CondExpr {
-
-    void m() {
-        @Peer Object o = true ? new @Peer Object() : this;
-    }
-}
